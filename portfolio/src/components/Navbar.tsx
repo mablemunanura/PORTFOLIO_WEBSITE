554f19400727f2172c3ba89be846e72937e846f2
mablemunanura <mablemunanura@gmail.com>
2025-11-16 04:34:51 +0300

    navbar

--- portfolio/src/components/Navbar.tsx
+++ portfolio/src/components/Navbar.tsx
@@ -18,10 +18,10 @@ export default function Navbar() {
   ]
 
   return (
-    <nav className="fixed top-0 left-0 right-0 z-50 bg-transparent">
+    <nav className="fixed top-0 left-0 right-0 z-50 bg-[#BB9476]/40 backdrop-blur-md shadow-md">
       <div className="max-w-7xl mx-auto flex items-center justify-between px-4 sm:px-6 md:px-12 py-3">
         {/* Logo (small) */}
-        <h2 className="text-xl md:text-sm font-semibold text-black">
+        <h2 className="text-md md:text-xl font-semibold text-black">
           Mable <span className="text-[#264E36]">Tusiime</span>
         </h2>
 
