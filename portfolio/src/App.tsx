72498f432ef3869d7cfe3ea891f01687af072b54
mablemunanura <mablemunanura@gmail.com>
2025-12-09 15:15:34 +0300

    routes

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -9,6 +9,7 @@ function App() {
     <Router>
       <Navbar />
       <Routes>
+        <Route path="/" element={<Hero />} />
         <Route path="/home" element={<Hero />} />
         <Route path="/about" element={<About />} />
       </Routes>
